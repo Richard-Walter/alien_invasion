adc757736a368d2eef9034363fcf13e1697b11e6
Richard-Walter <willthisnamedo@gmail.com>
2019-03-10 09:14:45 +1100

    Added bullet functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,32 +1,29 @@
-import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
+
     # Initialize game and create a screen object
     pygame.init()
     ai_setting = Settings()
     screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
+    ship = Ship(ai_setting, screen)
     pygame.display.set_caption("Alien Invasion")
 
-    # Set the background color.
-    bg_color = (230, 230, 230)
+    # Make a group to store bullet in.
+    bullets = Group()
+
     # Start the main loop for the game.
     while True:
+        gf.check_events(ai_setting, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_setting, screen, ship, bullets)
 
-        # Watch for keyboard and mouse evenets
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_setting.bg_color)
-
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
 
 run_game()
 
--- settings.py
+++ settings.py
@@ -7,3 +7,14 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
+        # Ship settings
+        self.ship_speed_factor = 0.5
+
+        # Bullet Settings
+        self.bullet_speed_factor = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+
